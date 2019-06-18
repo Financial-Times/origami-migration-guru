1c2e0d2665e737ee0bbeb5c723c5c358ab812b41
notlee <lee.moody@ft.com>
2019-06-18 13:53:23 +0100

    Add JSDoc for Repo

--- src/repo.js
+++ src/repo.js
@@ -1,5 +1,9 @@
 const Dependency = require('./dependency');
 
+/**
+ * @param {*} registry - To validate is a supported registry name.
+ * @throws {Error}
+ */
 function validateRegistry(registry) {
 	if (!['bower', 'npm'].includes(registry)) {
 		throw new Error(`Registry ${registry} must be either "npm" or "bower".`);
@@ -7,6 +11,9 @@ function validateRegistry(registry) {
 }
 
 class Repo {
+	/**
+	 * @param {String} repoName - The name of the repository including organisation e.g. "Financial-Times/o-table".
+	 */
 	constructor(repoName) {
 		this.id = repoName;
 		[this.org, this.name] = repoName.split('/');
@@ -17,6 +24,14 @@ class Repo {
 		]);
 	}
 
+	/**
+	 * Get the name for this repo. Optionally by registry (bower or npm).
+	 * E.g. a repo "Financial-Times/example" would return "example" by default.
+	 * But it may be named differently in `bower.json` or `package.json`, e.g.
+	 * "@financial-times/example".
+	 * @param {String|null} registry [null] - The registry to get the repository name from `npm`, `bower`, or `null` for the repository name without organisation.
+	 * @return {String} - The repository's name.
+	 */
 	getName(registry = null) {
 		if (registry) {
 			validateRegistry(registry);
@@ -25,6 +40,11 @@ class Repo {
 		return this.name;
 	}
 
+	/**
+	 * Get the dependencies for this repo. Optionally by the registry they are included with (bower or npm).
+	 * @param {String|null} registry [null] - The registry to get dependencies for `npm`, `bower`, or `null` for either.
+	 * @return {Array<Dependency>} - The dependencies of this repo.
+	 */
 	getDependencies(registry = null) {
 		if (registry) {
 			validateRegistry(registry);
@@ -36,6 +56,11 @@ class Repo {
 		}, []);
 	}
 
+	/**
+	 * Add a manifest file to the repository and extract dependencies for the repo.
+	 * @param {String|null} registry - The registry the manifest is for `npm` or `bower`.
+	 * @param {String|Object} manifest - The manifest file as a JSON object or string.
+	 */
 	addManifest(registry, manifest) {
 		validateRegistry(registry);
 		manifest = typeof manifest === 'string' ? JSON.parse(manifest) : manifest;
